3088f9dc499c96beca6f8ab998be58cdda2f6d4b
Neha Mundase <106380291+neha-mundase20@users.noreply.github.com>
2024-07-21 11:49:50 +0530

    Time: 15 ms (94.67%), Space: 95.6 MB (19.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        multiset<int>st;
-        for(int i=0;i<nums1.size();i++){
-            st.insert(nums1[i]);
+        int i=0,j=0,k=0,m=nums1.size(),n=nums2.size();
+        vector<int>answer;
+
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                answer.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                answer.push_back(nums2[j]);
+                j++;
+            }
         }
-        for(int i=0;i<nums2.size();i++){
-            st.insert(nums2[i]);
+
+        while(i<m){
+            answer.push_back(nums1[i]);
+            i++;
         }
-        
-        vector<int>v;
-        
-        for(multiset<int>::iterator it=st.begin();it!=st.end();it++){
-            v.push_back(*it);
+
+        while(j<n){
+            answer.push_back(nums2[j]);
+            j++;
         }
-        
-        double median=0;
-        int p=v.size();
-        int n=v.size()-1;
-        
-        if(p%2!=0){ //size of vector is odd
-            median=v[(n/2)];
+
+
+        if((m+n)%2==0){ //even number of elements
+            double val1=double(answer[(m+n)/2])/2.0;
+            double val2=double(answer[((m+n)/2)-1])/2.0;
+            return val1+val2;
         }
-        else{   //size of vector is even
-            median=double(v[n/2]+v[(n/2)+1])/2;
-        }
-        return median;
+
+        return double(answer[(m+n)/2]);
     }
 };
