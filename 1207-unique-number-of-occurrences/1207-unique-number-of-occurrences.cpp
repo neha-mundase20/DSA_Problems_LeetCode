653957c6f4f77efd29188dc4d8b96ccf8033c384
Neha Mundase <106380291+neha-mundase20@users.noreply.github.com>
2024-09-14 12:28:15 +0530

    Time: 3 ms (56.97%), Space: 11.6 MB (40.01%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(auto it:arr){
+            mp[it]++;
+        }
+
+        unordered_set<int>st;
+        for(auto it:mp){
+            if(st.find(it.second)!=st.end()){
+                return false;
+            }
+            else{
+                st.insert(it.second);
+            }
+        }
+        return true;
+    }
+};
