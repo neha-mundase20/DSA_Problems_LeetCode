c1c6ff141ca780e767cad9c7114f7d18371e13be
Neha Mundase <106380291+neha-mundase20@users.noreply.github.com>
2024-11-24 21:29:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (26.01%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n=sentence.length()-1;
+        if(sentence[0]!=sentence[n]){
+            return false;
+        }
+
+        for(int i=0;i<sentence.length();i++){
+
+            if(sentence[i]==' '){
+                if(sentence[i-1]!=sentence[i+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
